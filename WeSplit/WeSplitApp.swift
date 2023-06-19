6bdada22286b3113fcdbfbdecf024c88d2839b70
Diego Perdomo <diegoperdomo94@gmail.com>
2023-06-19 16:22:38 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Diego Perdomo on 19/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
